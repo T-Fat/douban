cb49f7f1698738dcaf9229c6f11a166514404fb0
zyytFat <1415716687@qq.com>
2021-07-09 11:33:03 +0800

    就是玩

--- pa.py
+++ pa.py
@@ -17,15 +17,41 @@ def main():
     # 1.爬取网页
     datalsit = getData(baseurl)
     # 3.保存数据
-    savepath = ".\\豆瓣电影Top250.xls"
+    # savepath = ".\\豆瓣电影Top250.xls"
+    askURL("https://movie.douban.com/top250?start=")
 
 
 # 爬取网页
 def getData(baseurl):
     datalist = []
-    # 2.解析网页
+    for i in range(0, 10):  # 调用获取页面信息的函数25次 （左闭右开，但是正好250条）
+        url = baseurl + str(i * 25)
+        html = askURL(url)
+        # 2.逐一解析网页
     return datalist
 
+
+# 得到指定一个URL的网页内容
+def askURL(url):
+    # 模拟浏览器头部信息，向豆瓣服务器发送消息
+    head = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+    req = urllib.request.Request(url, headers=head)
+    html = ""
+    try:
+        response = urllib.request.urlopen(req)
+        html = response.read().decode("UTF-8")
+        print(html)
+    except Exception as e:
+        if hasattr(e, "code"):
+            print(e.code)
+        if hasattr(e, "reason"):
+            print(e.reason)
+    return html
+
+
+# 用户代理，表示告诉豆瓣服务器，我们是什么类型的机器，本质上是高速浏览器，我们可以接收什么水平的
 
 def saveData(savepath):
     print("save")
