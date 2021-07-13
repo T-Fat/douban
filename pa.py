ceb9fe909467672dad3a30659551d4d255805eb8
MrLi <lijinhappy@qq.com>
2021-07-13 22:51:40 +0800

    1

--- pa.py
+++ pa.py
@@ -18,16 +18,42 @@ def main():
     datalsit = getData(baseurl)
     # 3.保存数据
     # savepath = ".\\豆瓣电影Top250.xls"
-    askURL("https://movie.douban.com/top250?start=")
+    # askURL("https://movie.douban.com/top250?start=")
+
+
+# 生成正则表达式对象，表示规则（字符串的模式）
+findLink = re.compile(r'<a href="(.*?)">')
+# 影片图片
+findImgSrc = re.compile(r'<img.*src="(.*?)"', re.S)  # re.S让换行符包换在字符中
+# 影片标题
+findTitle = re.compile(r'<span class="title">(.*)</span>')
+# 影片评分
+findRating = re.compile(r'<span class="rating_num" property="v:average">(.*)</span>')
+# 评价人数
+findJudg = re.compile(r'<span>(\d*)人评价</span>')
+# 找到概况
+findInq = re.compile(r'<span class="inq">(.*)</span>')
+# 找到影片的相关内容
+findBd = re.compile(r'<p class="">(.*)</p>', re.S)
 
 
 # 爬取网页
 def getData(baseurl):
     datalist = []
-    for i in range(0, 10):  # 调用获取页面信息的函数25次 （左闭右开，但是正好250条）
+    for i in range(0, 1):  # 调用获取页面信息的函数25次 （左闭右开，但是正好250条）
         url = baseurl + str(i * 25)
         html = askURL(url)
         # 2.逐一解析网页
+        soup = BeautifulSoup(html, "html.parser")
+        for item in soup.find_all('div', class_="item"):  # 查找符合要求的字符串，形成列表
+            data = []  # 保存一部电影的全部信息
+            item = str(item)
+            print(item)
+            break
+            # 影片的详情页连接
+            link = re.findall(findLink, item)[0]  # re库通过正则表达式来查找指定的字符串
+            print(link)
+
     return datalist
 
 
@@ -42,7 +68,7 @@ def askURL(url):
     try:
         response = urllib.request.urlopen(req)
         html = response.read().decode("UTF-8")
-        print(html)
+        # print(html)
     except Exception as e:
         if hasattr(e, "code"):
             print(e.code)
